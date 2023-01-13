9ff36dcae9e176ea59b81babb0683dd45c37da86
jeffersondrs <jeffersonduarte@id.uff.br>
2023-01-13 13:07:39 -0300

    bug em importação com nome errado

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,4 +1,4 @@
-import Directory from "../../assets/components/directory/directory.component";
+import Directory from "../../assets/components/directory/Directory.component";
 import Categories from "../../categories";
 function Home() {
   return <Directory categories={Categories} />;
